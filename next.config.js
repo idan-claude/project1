2c04202eed868afa061d070a8868102783d9751c
idan yehiel <idanyehiel@MacBook-Air-sl-idan.local>
2026-05-23 23:25:41 +0300

    Update next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ae01.alicdn.com' },
+      { protocol: 'https', hostname: 'ae02.alicdn.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'via.placeholder.com' },
+    ],
+  },
+}
+
+module.exports = nextConfig
