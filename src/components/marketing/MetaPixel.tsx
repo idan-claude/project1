8b1ed4adb6320272953e6c2c1076b0aa153039a7
idan yehiel <idanyehiel@MacBook-Air-sl-idan.local>
2026-05-27 03:33:56 +0300

    Update MetaPixel.tsx

---
+++ src/components/marketing/MetaPixel.tsx
@@ -0,0 +1,62 @@
+'use client'
+import Script from 'next/script'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+interface MetaPixelProps {
+  pixelId: string
+}
+
+export default function MetaPixel({ pixelId }: MetaPixelProps) {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  // Track PageView on route changes
+  useEffect(() => {
+    if (window.fbq) {
+      window.fbq('track', 'PageView')
+    }
+  }, [pathname, searchParams])
+
+  if (!pixelId) return null
+
+  return (
+    <>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${pixelId}');
+fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  )
+}
+
+declare global {
+  interface Window {
+    fbq: (action: string, event: string, params?: Record<string, unknown>, options?: Record<string, unknown>) => void
+    _fbq?: unknown
+  }
+}
