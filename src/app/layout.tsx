8065d0e612378528fdc63ff53e45afcf8440e716
idan yehiel <idanyehiel@MacBook-Air-sl-idan.local>
2026-06-03 10:40:34 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <MetaPixel pixelId={process.env.NEXT_PUBLIC_META_PIXEL_ID} />
           </Suspense>
         )}
+        </Providers>
       </body>
     </html>
   )
